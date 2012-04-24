a7ea17187d83f08ecaba5a7b363e1c57aec63ec6
tai <zzwzzhao@163.com>
2012-04-24 20:18:01 +0800

    startgit add .git add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DotaForum::Application.config.secret_token = '4daf9c211138e03bd16a2bfb81cc6050f93a8357364a8565fc9f7fa1623ff1dcdd7e2113287ccbc257eeedc247fcf840d0ec25e1e2c2306d274a338fee308a70'
