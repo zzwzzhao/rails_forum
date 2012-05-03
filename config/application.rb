99d8278f4e91637ddb92e01af0fd3cbd67acb899
tai <zzwzzhao@163.com>
2012-05-03 23:20:08 +0800

    asset config

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,8 @@ module DotaForum
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
 
